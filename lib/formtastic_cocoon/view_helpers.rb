65887083e264e182cc6dce3badd20b15f79a45fb
Manin Iliy <bkmzvf@gmail.com>
2011-01-29 10:02:09 +0600

    fix name ID for html element

--- lib/formtastic_cocoon/view_helpers.rb
+++ lib/formtastic_cocoon/view_helpers.rb
@@ -26,8 +26,7 @@ module FormtasticCocoon
     #
     def link_to_add_association(name, f, association)
       new_object = f.object.class.reflect_on_association(association).klass.new
-      model_name = new_object.class.name.underscore
-      hidden_div = content_tag('div', :id => "#{model_name}_fields_template", :style => "display:none;") do
+      hidden_div = content_tag('div', :id => "#{association.to_s.singularize}_fields_template", :style => "display:none;") do
         render_association(association, f, new_object)
       end
       hidden_div.html_safe + link_to(name, '#', :class => 'add_fields', :'data-association' => association.to_s.singularize)
